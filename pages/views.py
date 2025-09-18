e1a9c030155f6eb4a0b973b0c12eee503f6a8a78
JhonCayas <JCayas735@gmail.com>
2025-09-18 19:51:10 +0800

    Created css and hello with parameter

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        'title' : 'Home',
+        'features' : [
+            'DJango', 
+            'Templates', 
+            'Static Files'
+        ]
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', { 'title':'About' })
+
+def hello(request, name):
+    return render(request, 'hello.html', {'name':name})
