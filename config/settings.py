628f302533fb2997997aff78d3c0768a61f94d54
JhonCayas <JCayas735@gmail.com>
2025-09-19 20:53:48 +0800

    Added 404 and 500

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-slrhs8)bo$mbc!juk)i_5&*w7uk)!&u2@(w+m+8=k_*401202@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
